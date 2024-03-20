b7049c3e8fc29ece6abf7f76baf9debf16dea61b
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-20 13:19:35 +0300

    Python Checker Rectify

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Waits for a random time between 0 and max_delay seconds."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
