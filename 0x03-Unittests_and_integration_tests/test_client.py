34407bdef872927873594ed4e850adbf04a6c72c
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-12-16 10:08:57 +0300

    update client test code style

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -92,7 +92,9 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def test_public_repos_with_license(self):
         """Test public_repos with license filtering."""
         client = GithubOrgClient("test_org")
-        self.assertEqual(client.public_repos(license="apache-2.0"), self.apache2_repos)
+        self.assertEqual(
+            client.public_repos(license="apache-2.0"), self.apache2_repos
+        )
 
 
 if __name__ == "__main__":
